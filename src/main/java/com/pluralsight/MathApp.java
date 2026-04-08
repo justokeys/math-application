6664d675e6c978a23db1ac736bf6d942bf0e994b
JustinK <productivekeys@gmail.com>
2026-04-08 15:51:35 -0500

    fisrt

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -4,8 +4,28 @@ public class MathApp {
     public static void main(String[] args) {
         double bobSalary = 87094.34;
         double garySalary = 192033.67;
-        double highestSalary = Math.max(bobSalary,garySalary);
-        String
+        double highestSalary = Math.max(bobSalary, garySalary);
+        double carPrice = 267767.69;
+        double truckPrice = 125000.35;
+        double highestCarPrice = Math.max(carPrice,truckPrice);
 
-        System.out.println();
+        System.out.println("The highest priced car is " + highestCarPrice + "\n");
+
+        System.out.println("The highest salary is " + highestSalary + "\n");
+
+        double  radius = 7.25;
+
+        double area = Math.PI * Math.sqrt(radius);
+
+        System.out.println("The radius is " + area);
+
+
+
+
+
+
+    }
+
+
+
 }
