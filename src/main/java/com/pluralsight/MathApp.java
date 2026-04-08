9c0d58d59ebf8c65d2172deea1817468eade0d00
JustinK <productivekeys@gmail.com>
2026-04-08 15:25:54 -0500

    fisrt

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        double bobSalary = 87094.34;
+        double garySalary = 192033.67;
+        double highestSalary = Math.max(bobSalary,garySalary);
+        String
+
+        System.out.println();
+}
